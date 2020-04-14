01d9ff08a1d3fb13f9b6e92578a619fe794660ac
SHANKAR KUMAR S <56698590+rampage-rider@users.noreply.github.com>
2020-04-14 22:58:07 +0530

    Add files via upload

---
+++ head_start.sql
@@ -0,0 +1,12 @@
+insert into users values('user1','user1@gmail.com',1000);
+insert into users values('user2','user2@gmail.com',1000);
+
+insert into liked values('user1@gmail.com','tt5817168', 'Terminator');
+insert into liked values('user1@gmail.com','tt7286456', 'Joker');
+insert into liked values('user1@gmail.com','tt0119654', 'Men in Black');
+insert into liked values('user1@gmail.com','tt0107290', 'Jurassic Park');
+
+insert into liked values('user2@gmail.com','tt5817168', 'Terminator');
+insert into liked values('user2@gmail.com','tt0499549', 'Avatar');
+insert into liked values('user2@gmail.com','tt0119654', 'Men in Black');
+insert into liked values('user2@gmail.com','tt0107290', 'Jurassic Park');
